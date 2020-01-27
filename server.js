9e5f69731a8afeecac688209ea2ea9597d9d49a2
Simon Milz <simon@milzer.de>
2020-01-27 13:49:58 +0100

    removed bodyparser

--- server.js
+++ server.js
@@ -2,13 +2,10 @@ const express = require('express')
 const app = express()
 const port = 1337
 const cors = require('cors')
-const bodyParser = require('body-parser')
 const persons = require('./persons')
 
 // Configure web-server
 app.use(cors())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
 
 app.get('/api/persons', (req, res) => {
 	res.json({
